dae87c66238c9e1861637abb6a6fb82126d947f7
Uelia <uelidae@outlook.com>
2021-08-31 07:10:26 -0400

    Add read time calculations

--- word-count/js/script.js
+++ word-count/js/script.js
@@ -2,4 +2,11 @@ function count_words() {
     let text = document.getElementById("textbox").value;
     document.getElementById("char-count-value").innerHTML = text.length;
     document.getElementById("word-count-value").innerHTML = text.match(/([\w]+)/g).length;
-}
+
+    // Calculate read time
+    time = math.round(text.value / 2.5) / 10;
+    hours = math.floor(time / 3600)
+    minutes = math.floor(time / 60);
+    seconds = time % 60;
+    document.getElementById("read-time-value").innerHTML = `${hours}h ${minutes}m ${seconds}s`;     
+    }
